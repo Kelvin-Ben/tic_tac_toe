ff12deff30ec10e06291b192228fd9322b976c73
Kelvin-Ben <kevoben14@gmail.com>
2023-06-21 23:10:54 +0300

    Add a function to handle cell click

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let board = ['','','','','','','','',''];
 let currentPlayer = 'X';
-let getActive = true;
+let gameActive = true;
 
 const winningCombinations = [
   [0,1,2], [3,4,5], [6,7,8],
   [0,3,6], [1,4,7], [2,5,8],
   [0,4,8], [2,4,6]
 ];
+
+function handleCellClick(cell, cellIndex) {
+  if (board[cellIndex] !== '' || !gameActive) {
+    return;
+  }
+}
