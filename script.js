4a79f923e5d1eb83e582fe5d0545dfebd1b21ef1
Kelvin-Ben <kevoben14@gmail.com>
2023-06-21 23:07:32 +0300

    Add winning combinations

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let board = ['','','','','','','','',''];
 let currentPlayer = 'X';
 let getActive = true;
+
+const winningCombinations = [
+  [0,1,2], [3,4,5], [6,7,8],
+  [0,3,6], [1,4,7], [2,5,8],
+  [0,4,8], [2,4,6]
+];
