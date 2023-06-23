39e78e2e4c06a503c96d1462efe9f7576a045066
Kelvin-Ben <kevoben14@gmail.com>
2023-06-23 08:20:42 +0300

    function to handle user moves

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ let board = [
 ]
 let currentPlayer = 'X';
 let gameOver = false;
+
+// function to handle user moves
+function handleMove(cellId) {
+  if (gameOver) return; // return if the game is already over
+
+  const row = Math.floor((cellId - 1) / 3);
+  const col = (cellId - 1) % 3;
+  if  (board[row][col] === '') {
+    board[row][col] = currentPlayer;
+    document.getElementById(cellId).textContent = currentPlayer;
+
+    checkGameOver();
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+
+    if (!gameOver) 
+      makeAIMove();
+  }
+}
